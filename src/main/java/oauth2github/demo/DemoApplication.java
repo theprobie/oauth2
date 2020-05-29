fa3ac1596e79804bc3c54e81312386b5be52411b
yangyy <1223770491@qq.com>
2020-05-29 21:55:45 +0800

    first commit

--- src/main/java/oauth2github/demo/DemoApplication.java
+++ src/main/java/oauth2github/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package oauth2github.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -10,6 +11,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+//        System.out.println(new BCryptPasswordEncoder().encode("123"));
     }
 
     @Bean
